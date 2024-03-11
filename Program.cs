278152605b31f69de8742a10dd0534c99bcb8003
Alex Isvoranu <alx.isvoranu@yahoo.com>
2024-03-11 14:28:55 +0200

    push

--- Program.cs
+++ Program.cs
@@ -10,8 +10,9 @@ builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 
 var app = builder.Build();
 
