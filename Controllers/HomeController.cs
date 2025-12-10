90af411f5cc91b05118abc4bb525934ead50e813
Alex Isvoranu <alx.isvoranu@yahoo.com>
2025-12-10 13:14:25 +0200

    home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -48,6 +48,11 @@ namespace Licenta3.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
